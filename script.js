d87597f91c4df3a2b48250320cd64b3d59c9eda6
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-04 12:12:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let button=document.querySelector('input[type="button"]');
+button.addEventListener('click',e=>{
+	e.preventDefault();
+	let element=document.getElementById('colorSelect');
+	element.value="Red";
+})
